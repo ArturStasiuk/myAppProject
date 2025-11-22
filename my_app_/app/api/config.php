25bad409720ab1b80d4b78f2a41a1f02c720a88f
Artur <arturstasiuk153@gmail.com>
2025-11-22 23:10:49 +0100

    cdcd

--- my_app_/app/api/config.php
+++ my_app_/app/api/config.php
@@ -3,8 +3,8 @@ class Config {
     //  konfiguracja dla lenowo
     const DB_HOST = 'localhost';
     const DB_NAME = 'project_002';
-    const DB_USER = 'root';
-    const DB_PASS = ''; 
+    const DB_USER = 'phpmyadmin';
+    const DB_PASS = 'artur'; 
     //bbb 35t34t 43t 
 
 /* configuracja dla windows
